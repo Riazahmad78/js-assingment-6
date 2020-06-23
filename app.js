f39c775ea251f67bbf8c1d9c43603ab752d9e0e4
Riazahamd <idtech.fsd@gmail.com>
2020-06-23 23:41:27 +0500

    js code

---
+++ app.js
@@ -0,0 +1,155 @@
+// chapter 21 to 25
+// task 1
+
+// var fName=prompt("Enter your first Name")
+// var lName=prompt("Enter your last name")
+
+// var fullName=fName+" "+lName
+// alert("Hello"+" "+fullName)
+// task 2
+// var mobileName = prompt("enter your mobile modelname"," Samsung Galaxy Edge 6S Plus")
+// var strLenght=mobileName.length;
+// document.write("My Favourite Phone is:"+mobileName+"<br>"+"The Lenght of the string is:s"+strLenght)
+// task 3
+// var text="pakistani"
+// var induxNumber=text.indexOf('n')
+// document.write("Index of 'n' is: "+induxNumber)
+// task 4
+// var srt="Hello World"
+// var indexNum=srt.lastIndexOf('l')
+// document.write("Last index of 'l': "+indexNum)
+// task5
+// var text="Pakistani"
+// var charAtIndex=text.charAt(3)
+// document.write("Character at index 3: "+charAtIndex)
+// task6
+// var str1 = prompt('Enter your firsrt name')
+// var str2 = prompt("Enter your last name");
+// var fullName = str1.concat(str2);
+// document.write("Hello "+fullName)
+// task7
+// var city="Hyderabad";
+// var newText = city.replace("Hyder", "Islam");
+// document.write("City :"+city)
+// document.write("<br>"+"After replacement: "+newText)
+// task 8
+// var message = "Ali and Sami are best friendsThey play cricket and football together."
+// var newText = message.replace(/and/g, "&");
+// document.write(newText)
+// task 9
+// var str="472"
+// var value=parseInt(str)
+// document.write("Value: "+str+"<br>"+"Type: "+"String: "+"<br>"+"Value: "+value+"<br>"+"Type: "+"Number")
+// task 10
+// var text=prompt("User input","peanuts")
+// var textUpperCase=text.toUpperCase()
+// document.write("User input: "+text+"<br>"+"Upper case: "+textUpperCase)
+// task 11
+// var text=prompt("User input","javascript")
+// var text1=text.slice(0,1)
+// var newtext=text1.toUpperCase()
+// var otherChar=text.slice(1)
+// document.write("User input: "+text+"<br>"+"Title case: "+ newtext+otherChar)
+// task 12
+// var  num=35.36;
+// var srt=num.toString().replace(".","")
+// document.write("Number: "+num+"<br>"+"Result: "+srt)
+// task 14
+// var arr = ['cake', 'apple pie', 'cookie', 'chips', 'patties']
+// var item=prompt("Enter your bakery item")
+// var itemLowerCase= item.toLowerCase()
+// var item =itemLowerCase
+// var arr = ['cake', 'apple pie', 'cookie', 'chips', 'patties']
+// for (var i=0 ; i<arr.length; i++){
+//     if (arr[i]==item){
+//         alert("item has found")
+//         break;
+//     }
+//     else{
+//         alert("Item has not found")
+//     }
+// }
+
+// task 16
+// var university = "University of Karachi";
+// var res=university.split("")
+// document.write(res)
+// // task 17
+// var text="Pakistan"
+// var charAtIndex=text.slice(-1)
+// document.write("User Input: "+text+"<br>"+"Last Character of input: "+charAtIndex)
+
+// Task 18
+// function countOccurences(string, word) {
+//     return string.split(word).length - 1;
+//  }
+//  var text="The quick brown fox jumps over thelazy dog"; 
+//  var newText=text.toLowerCase()
+//  var count=countOccurences(newText,"the"); // 2
+//  document.write("Text:" +"The quick brown fox jumps over thelazy dog"+"<br>"+"There are "+count+"ocurrences of the world 'the'")
+//  Assignment # 26-30
+// task 1
+// var num=prompt("Enter a positive integer","3.45214")
+// var numRoundOff=Math.round(num);
+// var numFloor=Math.floor(num);
+// var numceil=Math.ceil(num);
+// document.write("number: "+num+"<br>"+"round off value: "+numRoundOff+"<br>"+"Floor value: "+numFloor+"<br>"+"numceil: "+numceil)
+// task2
+// var num=prompt("Enter a negative integer","-2.673")
+// var numRoundOff=Math.round(num);
+// var numFloor=Math.floor(num);
+// var numceil=Math.ceil(num);
+// document.write("number: "+num+"<br>"+"round off value: "+numRoundOff+"<br>"+"Floor value: "+numFloor+"<br>"+"numceil: "+numceil)
+// task3
+// var num=prompt("Enter value","-4")
+// num1=Math.abs(num);
+// document.write("The absolute value of "+num+" is "+num1);
+// task4
+// var randomDiceNum=Math.random()
+// randomDiceNum1=(randomDiceNum*6)+1;
+// var numberOfStars=Math.floor(randomDiceNum1)
+// document.write("random Dice Value: "+numberOfStars);
+
+// task5
+
+// task6
+// var num=Math.random()*100
+// num=Math.round(num)
+// document.write("random number between 1 and 100: "+num);
+// // task7
+// var w=prompt("enter weight",50kgs)
+//  w=parseInt(w)
+//  document.write(w)
+// //  task8
+// var Number=prompt("enter number between 1 and 10")
+//   secretNumber1=Math.random(Number)*10
+// secretNumber1=Math.round(Number)
+//   if ()
+
+
+
+
+
+
+
+  // Assignment 31 to 34
+  // task1
+  // var a=new Date()
+  // document.write(a)
+  // task 3
+//   var d = new Date();
+// a = d.getDay();
+//   switch (new Date().getDay()) {     
+//     case 6||'sat':                         // if (day == 6)
+//         text = "Saturday";          
+//         break;
+//     case 0||'sun':                         // if (day == 0)
+//         text = "Sunday";
+//         break;
+//     default:                        // else...
+//         text = "Whatever";
+// } 
+
+// alert(text)
+  // task4
+  
